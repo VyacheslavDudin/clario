a9a2bd7bed349f34f09a5307d28864796e98726a
Vyacheslav Dudin <slavak.dudin2@gmail.com>
2023-10-26 23:07:43 +0300

    fix: change image, use dynamic current year

--- src/layouts/Footer.tsx
+++ src/layouts/Footer.tsx
@@ -1,13 +1,17 @@
-import { Center, Stack, Text } from "@mantine/core";
+import { Center, Stack } from "@mantine/core";
+
+import { Text } from "src/components";
 import { ReactComponent as Logo } from "src/assets/images/logo.svg";
 
 export function Footer() {
+  const currentYear = new Date().getFullYear();
+
   return (
     <Center pt="xl" pb="md" bg="main.0">
       <Stack align="center" gap="xs" px="lg">
         <Logo />
         <Text ta="center">
-          © 2023 Clario Tech DMCC
+          © {currentYear} Clario Tech DMCC
           <br />
           All rights reserved.
         </Text>
